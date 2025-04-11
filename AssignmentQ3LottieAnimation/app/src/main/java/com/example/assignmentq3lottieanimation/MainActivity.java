17f2b03e22840cf411aed938591bf6ee5f3f569c
Anmol1-stack <anmol.chaturvedi.23cse@bmu.edu.in>
2025-04-12 03:04:27 +0530

    Added Q1 and Q3 Android Studio projects

---
+++ AssignmentQ3LottieAnimation/app/src/main/java/com/example/assignmentq3lottieanimation/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.assignmentq3lottieanimation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LottieAnimationView animationView = findViewById(R.id.lottieView);
+        animationView.setAnimation("animation.json");
+        animationView.playAnimation();
+    }
+}
